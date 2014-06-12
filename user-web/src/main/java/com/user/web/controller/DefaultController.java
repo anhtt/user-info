822f205dfef4a2c150139a453792fd0a9cd5d992
congnt <ntcong85@gmail.com>
2014-06-12 18:19:17 +0700

    third commit

---
+++ user-web/src/main/java/com/user/web/controller/DefaultController.java
@@ -0,0 +1,7 @@
+package com.user.web.controller;
+
+/**
+ * Created by congnt on 6/12/14.
+ */
+public class DefaultController {
+}
---
+++ user-web/src/main/java/com/user/web/model/UserInfoController.java
@@ -0,0 +1,7 @@
+package com.user.web.model;
+
+/**
+ * Created by congnt on 6/12/14.
+ */
+public class UserInfoController {
+}
